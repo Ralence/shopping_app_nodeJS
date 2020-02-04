25899f7ca123dfa44c4678ddd6cae4d8d6de9ea4
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-02-04 21:53:41 +0100

    feat: implement pug template

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(shopRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageNotFound: 'Page not found 404', docTitle: '404 not found' });
 });
 
 app.listen(3000);
