7e46f17644e001fa87d67a86ff7ce63a7ae973b7
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-03-07 18:37:43 +0100

    feat: extract productId from routh params

--- controllers/shop.js
+++ controllers/shop.js
@@ -11,6 +11,12 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const productId = req.params.productId;
+
+  res.redirect('/');
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
