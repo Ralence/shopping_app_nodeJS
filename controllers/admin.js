6bbb12f3aa9952cf26257e2637429a1597e3e09a
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-03-10 19:14:25 +0100

    feat: add and connect edit-product view

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,11 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false,
+  });
 };
 
 exports.postAddProduct = (req, res, next) => {
@@ -9,6 +13,25 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  const productId = req.params.productId;
+
+  if (!productId || !editMode) {
+    return res.redirect('/');
+  }
+
+  Product.findById(productId, product => {
+    // TODO check if no product
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product,
+    });
+  });
 };
 
 exports.getProducts = (req, res, next) => {
