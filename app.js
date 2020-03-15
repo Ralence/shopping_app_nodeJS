48c5e90ae9799b7723e769d575ad56df415f4b6b
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-03-15 16:37:31 +0100

    feat: add mySql db

--- app.js
+++ app.js
@@ -8,10 +8,16 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 
+const db = require('./utils/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+db.execute('SELECT * FROM products')
+  .then()
+  .catch();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
---
+++ utils/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'my_first_node_schema',
+  password: '0310984r',
+});
+
+module.exports.pool = pool.promise();
