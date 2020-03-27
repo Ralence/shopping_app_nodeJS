66af6874cb05f79609fb282eae8b5464524e9015
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-03-27 20:04:40 +0100

    feat: set up getCart

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-  //.sync()
+  //.sync({ force: true })
+  .sync()
   .then(result => {
     return User.findByPk(1);
     // console.log(result);
@@ -54,6 +54,9 @@ sequelize
       return User.create({ name: 'Rale', email: 'test@test.com' });
     }
     return user;
+  })
+  .then(user => {
+    return user.createCart();
   })
   .then(user => {
     // console.log(user);
--- controllers/shop.js
+++ controllers/shop.js
@@ -42,7 +42,21 @@ exports.getIndex = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-exports.getCart = (req, res, next) => {};
+exports.getCart = (req, res, next) => {
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts();
+    })
+    .then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products,
+      });
+    })
+    .catch(err => console.log(err));
+};
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
