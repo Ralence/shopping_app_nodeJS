f7b4eb93d8361622ea072c89ff43f2602b1e27ae
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-03-28 13:11:26 +0100

    feat: add product to cart and retreive the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -60,11 +60,41 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-  const price = req.body.productPrice;
-  console.log(productId);
-  Cart.addProduct(productId, price);
-  res.redirect('/cart');
-  //Product.findById(productId, ())
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: productId } });
+    })
+    .then(products => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      let newQuantity = 1;
+      if (product) {
+        // TODO
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return fetchedCart.addProduct(product, {
+          through: {
+            quantity: newQuantity,
+          },
+        });
+      }
+      return Product.findByPk(productId)
+        .then(product => {
+          return fetchedCart.addProduct(product, {
+            through: { quantity: newQuantity },
+          });
+        })
+        .catch(err => console.log(err));
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
